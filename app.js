263e698879d274d3b9e55611f9952eb6e13f5503
paul bratslavsky <paulbratslavsky@pauls-MacBook-Pro.local>
2018-11-18 18:11:58 -0500

    Added Delete and fileter

--- app.js
+++ app.js
@@ -20,41 +20,93 @@ console.log(taskInput);
 LoadAllEventListeners();
 
 function LoadAllEventListeners() {
-    // Add Tasks To Form
+    // Add Tasks To Form event
     form.addEventListener('submit', addTask);
+    // Remove Task From List event
+    taskList.addEventListener('click', removeTask);
+    // Clear All Tasks event
+    clearButton.addEventListener('click', clearAllTasks);
+    // Filter Tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
-
 function addTask(e) {
     if ( taskInput.value === '' ) {
         alert("Please Add Todo");
+    } else {
+        e.preventDefault();
+
+        // Create li element
+        const li = document.createElement('li');
+        
+        // Add Class
+        li.className = 'collection-items';
+        
+        // Create Text Node and append 
+        li.appendChild(document.createTextNode(taskInput.value));
+        console.log(taskInput.value, 'this is the task input value');
+
+        // Create new link element 
+        const button = document.createElement('button');
+
+        // Add class Name 
+        button.className = 'delete-item';
+
+        // Create inner html
+        button.innerHTML = 'Delete Item';
+        
+        
+        li.appendChild(button);
+        console.log(li);
+        // Append ul to the ul
+
+        taskList.appendChild(li);
+
+        taskInput.value = '';
     }
-    e.preventDefault();
+}
 
-    // Create li element
-    const li = document.createElement('li');
+// Remove Task
+function removeTask(e) {
+    if ( e.target.classList.contains('delete-item') ) {
+        console.log('Remove Task Button Cliked');
+
+        // Remove Li
+        if ( confirm('Are you sure?') ) {
+            e.target.parentElement.remove();
+        }
+    }
     
-    // Add Class
-    li.className = 'collection-items';
-    
-    // Create Text Node and append 
-    li.appendChild(document.createTextNode(taskInput.value));
-    console.log(taskInput.value, 'this is the task input value');
+}
 
-    // Create new link element 
-    const button = document.createElement('button');
+// Clear All
+function clearAllTasks() {
+    console.log("Clear all button clicked"); 
+    while ( taskList.firstChild ) {
+        taskList.removeChild(taskList.firstChild);
+    }
 
-    // Add class Name 
-    button.className = 'delete-item';
+}
 
-    // Create inner html
-    button.innerHTML = 'Delete Item';
-    
-    
-    li.appendChild(button);
-    console.log(li);
-    // Append ul to the ul
+// Function Filter
+function filterTasks(e) {
+    console.log("Key Pressed");
 
-    taskList.appendChild(li);
+    const text = e.target.value.toLowerCase();
+    console.log(text);
+
+    const allItems = document.querySelectorAll('li');
+    console.log(allItems, 'all items');
+
+    allItems.forEach( function(task) {
+        const item = task.firstChild.textContent;
+        console.log(item, 'from fiter');
+
+        if ( item.toLowerCase().indexOf(text) != -1 ) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none'
+        }
+    });
 }
